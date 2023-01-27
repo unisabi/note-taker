e94dda494b4aafc1fdb7db4c966863b3fa10aaee
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-01-27 02:38:38 -0800

    fixed note page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,7 +5,7 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'../public/index.html'))
   });
 
-router.get('/', function (req, res){
+router.get('/notes', function (req, res){
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 
 });  
